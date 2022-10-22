b1e7b12f8c900f3d0af4111c73cb17a800514e28
SunSh1eld <ustyanherman@gmail.com>
2022-10-22 22:25:04 +0200

    Added stuff

---
+++ Assets/Plugins/VNEngine/Scripts/Runtime/Core/Graphs/Data/Elements/Ports/PortsDictionary.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace VNEngine.Runtime.Core.Graphs.Data.Elements.Ports
+{
+    [Serializable]
+    public class PortsDictionary : SerializableDictionary<int, INPort>
+    {
+    }
+}
